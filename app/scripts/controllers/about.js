07b3298db03afd17d893c4634e65c0675a7ab95d
lxg <lxg1986@gmail.com>
2014-10-08 14:05:04 +0800

    update with grunt

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hongjiboyuanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hongjiboyuanApp
+ */
+angular.module('hongjiboyuanApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hongjiboyuanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hongjiboyuanApp
+ */
+angular.module('hongjiboyuanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
